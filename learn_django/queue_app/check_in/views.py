0622d4e319f72a67352704060a3ff321ae98a963
NH <thuha.nguyenp@gmail.com>
2021-09-05 14:20:37 +0700

    coffee machine

--- learn_django/queue_app/check_in/views.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-# Create your views here.
-from django.http import HttpResponse
-
-def index(request):
-  return HttpResponse('Hello, world. You\'re at the checkin page.')
